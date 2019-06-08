3805f51eab84e35a3651fbebb914a2229f8c00b3
ozrlz <oscar.cucei@gmail.com>
2019-06-08 16:21:32 -0500

    [IMP] Add Dress class

---
+++ models/dresses.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+class Dress:
+
+    def __init__(self, size, style, color, brand, material, long):
+        self.size = size
+        self.style = style
+        self.color = color
+        self.brand= brand
+        self.material= material
+        self.size = long
+
+    def getJson(self):
+        return vars(self)
+
+        
